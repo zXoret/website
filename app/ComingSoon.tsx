d46a361af28e8aa28a3fbf390466cd9d82341e49
zXoret <bluebrrykidfam@gmail.com>
2025-12-14 12:56:01 -0500

    Update ComingSoon message for FlarixMC launch

--- app/ComingSoon.tsx
+++ app/ComingSoon.tsx
@@ -2,8 +2,8 @@ export default function ComingSoon() {
     return (
         <div className={`z-5 fixed bottom-0 lg:bottom-auto lg:top-0 left-0 w-full bg-blue-500 text-white py-2 px-4`}>
         <p className="text-center">
-          BridgeSplash Minecraft Network is coming soon!<br />
-          Early access to SMP & Oneblock is available now.
+          FlarixMC Minecraft Network is coming soon!<br />
+          Early access to SMP & Oneblock is available soon.
         </p>
       </div>
     )
